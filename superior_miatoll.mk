86e1d0f7f52ea94563c530b0be4f4b00128ef2bb
Your Name <you@example.com>
2024-01-04 05:15:33 +0000

    miatoll : Bringup @SuperiorExtended

--- superior_miatoll.mk
+++ superior_miatoll.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2022 The LineageOS Project
+# Copyright (C) 2021-2022 The SuperiorOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,27 +8,42 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common SuperiorOS stuff.
+# Inherit some common SuperiorExtended stuff.
 $(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-
-# SuperiorOS stuff
-SUPERIOR_OFFICIAL := true
-TARGET_INCLUDE_PIXEL_CHARGER := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_USES_AOSP_RECOVERY := true
 
 PRODUCT_NAME := superior_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := SM6250
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# MiuiCamera
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
+
+# Inherit some common SuperiorExtendedOS stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_MATLOG := false
+TARGET_INCLUDE_PIXEL_CHARGER := true
+USE_MOTO_CALCULATOR := true
+TARGET_SUPPORTS_BLUR := true
+SUPERIOR_UDFPS_ANIMATIONS := false
+USE_MOTO_CLOCK := true
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+BUILD_WITH_GAPPS := false
+USE_QUICKPIC := true
+USE_DUCKDUCKGO := false
+
+# Maintainer
+PRODUCT_SYSTEM_PROPERTIES += \
+     ro.spos.maintainer=AtharvaSwamy
+
+# Build Prop
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="miatoll_global-user 12 SKQ1.211019.001 V14.0.4.0.SJWMIXM release-keys"
 
---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/SuperiorOS-Devices/vendor_xiaomi_miatoll vendor/xiaomi/miatoll
+git clone https://github.com/SuperiorOS-Devices/kernel_xiaomi_sm6250 kernel/xiaomi/sm6250
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21.0 hardware/xiaomi
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-21.0 packages/resources/devicesettings
+git clone https://github.com/LineageOS/android_hardware_sony_timekeep -b lineage 21 hardware/sony/timekeep
+git clone https://github.com/Positron-B/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
