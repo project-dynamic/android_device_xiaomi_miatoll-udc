256b2cd6cc8536fdd01353ca5cdd93ac0d2caecc
Atharva Swamy <111036482+athizz2005@users.noreply.github.com>
2024-01-05 18:59:46 +0530

    miatoll: update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/project-dynamic/android_vendor_xiaomi_miatoll-udc -b 14-superior-clarence vendor/xiaomi/miatoll
-git clone https://github.com/SuperiorOS-Devices/kernel_xiaomi_sm6250 kernel/xiaomi/sm6250
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21.0 hardware/xiaomi
-git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-21.0 packages/resources/devicesettings
-git clone https://github.com/LineageOS/android_hardware_sony_timekeep -b lineage 21 hardware/sony/timekeep
+git clone https://github.com/SuperiorOS-Devices/kernel_xiaomi_sm6250 kernel/xiaomi/sm6250 --depth=1
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-21 packages/resources/devicesettings
+git clone https://github.com/LineageOS/android_hardware_sony_timekeep -b lineage-21 hardware/sony/timekeep
 git clone https://github.com/Positron-B/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
